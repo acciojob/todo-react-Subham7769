313b6e97896f0c6a1b4a82b7aa43adf2cf47b18b
Shubham Jain <shubham.rapariya2@gmail.com>
2023-11-18 02:02:06 +0530

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,16 +35,16 @@ const Header = () => {
         />
         <button type="submit">Add Task</button>
       </form>
-      <div>
+      <ul>
         {taskList.length === 0
           ? ""
           : taskList.map((taskName, index) => (
-              <div key={index} className='listItem' id={index}>
+              <li key={index} className='listItem' id={index}>
                 {taskName} 
                 <button onClick={()=>{deleteTask(index)}}>Delete</button>
-              </div>
+              </li>
             ))}
-      </div>
+      </ul>
     </div>
   );
 }
